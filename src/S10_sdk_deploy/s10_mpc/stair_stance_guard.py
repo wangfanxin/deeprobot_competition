df2fbe30d21a44e6f9654c0ad06ad9bd4b321c14
wfx <wfx@local>
2026-08-14 11:04:09 +0800

    debug(stair): body control trace

--- src/S10_sdk_deploy/s10_mpc/stair_stance_guard.py
+++ src/S10_sdk_deploy/s10_mpc/stair_stance_guard.py
@@ -84,6 +84,8 @@ class StairStanceGuard:
             pitch_err = -pitch
             tau_roll = kp_roll * roll_err - kd_roll * float(ang[0])
             tau_pitch = kp_pitch * pitch_err - kd_pitch * float(ang[1])
+            if __import__('os').environ.get('S10_STANCE_BODY_DEBUG', '0') == '1':
+                print(f'[BODY] roll={roll:.3f} pitch={pitch:.3f} ang={[round(float(v),3) for v in ang[:2]]} tau_roll={tau_roll:.1f} tau_pitch={tau_pitch:.1f}', flush=True)
             for i in range(4):
                 if not contact[i]:
                     continue
